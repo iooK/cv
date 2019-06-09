823814dfe291e437f14e94d26771abd077d24042
Kevin Lot <klot@keyclic.com>
2019-06-09 21:36:46 +0200

    Updates

---
+++ precache-manifest.a8369e1cda17972de2edc3cb0c0e9e79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e934bc90889be74a9b6d29fc453133",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2973a11487139236729c",
+    "url": "/static/css/main.d917be48.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "2973a11487139236729c",
+    "url": "/static/js/main.21658196.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
