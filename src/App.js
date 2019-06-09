5bff297454058e8e3ad58c2f915d02663f593aad
Kevin Lot <klot@keyclic.com>
2019-06-09 20:06:59 +0200

    New website.
    Nothing except a link to linkedin.

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section className="App-section">
+        <p>
+          Kevin Lot
+        </p>
+        <a
+          className="App-link"
+          href="https://fr.linkedin.com/in/kevinlot"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          linkedin link
+        </a>
+      </section>
+    </div>
+  );
+}
+
+export default App;
